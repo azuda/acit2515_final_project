088819ba68ac9b395e8e5c791907f1ed49e4ac16
azuda <aaronzhang587@gmail.com>
2022-11-22 15:35:51 -0800

    Update game screen

--- screens/game.py
+++ screens/game.py
@@ -5,3 +5,9 @@ from .base_screen import BaseScreen
 class GameScreen(BaseScreen):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
+
+
+  def run(self):
+    # get key presses outside of event loop
+      keys = pygame.key.get_pressed()
+      # handle controls
