035ad31f2c0621baa9fa36d1d71fb37bf2f8dda0
azuda <aaronzhang587@gmail.com>
2022-11-22 22:02:53 -0800

    Update game screen

--- screens/game.py
+++ screens/game.py
@@ -1,44 +1,56 @@
 import pygame
 from .base_screen import BaseScreen
 
-from components import Player
+from components import Player, Point
 
 
 class GameScreen(BaseScreen):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
 
-    self.player = Player(100, 100, limits=self.window.get_rect())
-
     self.sprites = pygame.sprite.Group()
+
+    # player sprite
+    self.player = Player(limits=self.window.get_rect())
     self.sprites.add(self.player)
+
+    self.point = Point(limits=self.window.get_rect(), type="300")
+    self.sprites.add(self.point)
+
+
+  def draw(self):
+    self.window.fill((25, 30, 35))
+    self.sprites.draw(self.window)
 
 
   def update(self):
     # movement keys
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-      self.player.move("left")
+      if keys[pygame.K_LSHIFT]:
+        self.player.move("left", boost=2.5)
+      else:
+        self.player.move("left")
     if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-      self.player.move("right")
-
-    # movement boost
-    if keys[pygame.K_LSHIFT]:
-      if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-        self.player.move("left")
-        self.player.move("left")
-      if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-        self.player.move("right")
+      if keys[pygame.K_LSHIFT]:
+        self.player.move("right", boost=2.5)
+      else:
         self.player.move("right")
 
+    # ensure point is always falling
+    self.point.move("down")
+
+    # player collision with point
+    if self.player.rect.colliderect(self.point.rect):
+      self.point.kill()
+      point = Point(limits=self.window.get_rect())
+      self.sprites.add(point)
+      # temporary redirect to start screen
+      # self.next_screen = "start"
+      # self.running = False
+
+    # update sprites
     self.sprites.update()
-
-    # handle catching points
-
-
-  def draw(self):
-    self.window.fill((20, 25, 30))
-    self.sprites.draw(self.window)
 
 
   def manage_event(self, event):
