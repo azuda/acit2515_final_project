cb6fea97ae4eb4fce60708b48f11fc3c680ad023
azuda <aaronzhang587@gmail.com>
2022-11-22 16:50:17 -0800

    Update player class

--- components/player.py
+++ components/player.py
@@ -1,11 +1,18 @@
 import pygame
+from .sprite import MySprite
 
 
-class Player(pygame.sprite.Sprite):
+class Player(MySprite):
   '''Component representing player character'''
 
-  def __init__(self, **kwargs):
+  def __init__(self, width, height, **kwargs):
     super().__init__(**kwargs)
-    self.image = pygame.image.load("../sprites/player.png")
+    self.image = pygame.image.load("./sprites/player.png")
+    self.image = pygame.transform.scale(self.image, (width, height))
     self.rect = self.image.get_rect()
-  
+
+    if self.limits:
+      self.move_to(
+        self.limits.center[0] - self.rect.width / 2,
+        self.limits.bottom - self.rect.height,
+      )
