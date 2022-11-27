77d1bae8ea400e2a6b27add6525aa1f9efd9070b
azuda <aaronzhang587@gmail.com>
2022-11-27 14:06:14 -0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import json
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+# read scores.json
+with open("scores.json", "r") as f:
+  scores = json.load(f)
+
+
+@app.route("/")
+def home():
+  return render_template("base.html", scores=scores), 200
