189b98b01e5a2f29bae8a5a9ec3b5e56fde37d7d
azuda <aaronzhang587@gmail.com>
2022-11-27 15:16:54 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 # read scores.json
-with open("scores.json", "r") as f:
+with open("data/scores.json", "r") as f:
   scores = json.load(f)
 
 
