fe8564a4c93701129816457aedf3d2a36b3a192c
azuda <aaronzhang587@gmail.com>
2022-11-28 22:46:45 -0800

    Add sort to flask app

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 import json
-from flask import Flask, render_template
+import handler.handler as handler
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
-# read scores.json
-with open("data/scores.json", "r") as f:
-  scores = json.load(f)
 
 
 @app.route("/")
 def home():
+  scores = handler.get_scores()
   return render_template("base.html", scores=scores), 200
+
+
+@app.route("/sort/<string:key>", methods=["GET"])
+def sort(key):
+  scores = handler.get_scores()
+  scores.sort(key=lambda x: x[key])
+  return render_template("sort.html", scores=scores, key=key), 200
 
 
 if __name__ == "__main__":
